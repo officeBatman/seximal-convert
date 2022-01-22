6056a8d479ddf14c4b4e4ff50a750f8b1f7f8ef6
Yonatan Reicher <yony252525@gmail.com>
2022-01-22 11:45:41 +0200

    Refactoring and looking better

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,8 @@ import Browser
 import Html exposing (Html)
 import Element exposing (..)
 import Element.Input as Input
+import Element.Font as Font
+import Element.Background as Background
 
 
 type alias Model =
@@ -57,32 +59,76 @@ update msg model =
 
 view : Model -> Html Msg
 view model = 
-    Element.layout [] (viewPage model)
+    Element.layout
+        [ Background.color colors.dark ]
+        (viewPage model)
 
 
 viewPage : Model -> Element Msg
 viewPage model = 
-    column []
-        [ Element.text "Hello World"
-        , Input.text []
-            { text = case model.input of
-                EmptyInput -> ""
-                PartialInput str -> str
-                NumberInput n -> String.fromInt n
-            , onChange = \s ->
-                    case s of
-                        "" -> ChangeInput EmptyInput
-                        "-" -> ChangeInput <| PartialInput s
-                        _ ->
-                            case String.toInt s of
-                                Nothing -> BadInput
-                                Just n -> ChangeInput <| NumberInput n
-            , label = Input.labelLeft [] (text "Enter a base-10 number")
-            , placeholder = Nothing
-            }
-        , row []
-            (case model.input of
-                NumberInput n -> [Element.text (stringFromIntInBase 6 n)]
-                _ -> [])
+    column
+        [ centerX ]
+        [ viewHeading model
+        , viewInput model
+        , viewOutput model
         ]
+
+
+viewHeading : Model -> Element Msg
+viewHeading model =
+    el
+        [ centerX
+        , padding 20
+        , Font.size 50
+        , Font.family [ Font.monospace ]
+        , Font.color colors.light
+        ]
+        (text "Seximal Converter")
+
+
+viewInput : Model -> Element Msg
+viewInput model =
+    Input.text []
+        { text = case model.input of
+            EmptyInput -> ""
+            PartialInput str -> str
+            NumberInput n -> String.fromInt n
+        , onChange = \s ->
+                case s of
+                    "" -> ChangeInput EmptyInput
+                    "-" -> ChangeInput <| PartialInput s
+                    _ ->
+                        case String.toInt s of
+                            Nothing -> BadInput
+                            Just n -> ChangeInput <| NumberInput n
+        , label =
+            Input.labelAbove
+                [ Font.color colors.light
+                , Font.family [ Font.monospace ]
+                ]
+                (text "enter a base-10 number")
+        , placeholder = Nothing
+        }
+
+
+viewOutput : Model -> Element Msg
+viewOutput model = 
+    row
+        [ centerX
+        , padding 50
+        , Font.color colors.saturated
+        , Font.size 40
+        , Font.extraBold
+        ]
+        (case model.input of
+            NumberInput n -> [Element.text (stringFromIntInBase 6 n)]
+            _ -> [])
+
+
+colors : { dark: Color, light: Color, saturated: Color }
+colors = 
+    { dark = rgb255 10 11 10
+    , light = rgb255 209 224 213
+    , saturated = rgb255 24 181 66
+    }
 
