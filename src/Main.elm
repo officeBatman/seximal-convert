8ab167a2e6363c2cb892ca21e9484ea6d49351d9
Yonatan Reicher <yony252525@gmail.com>
2022-01-22 12:09:54 +0200

    Increas/Decrease buttons

--- src/Main.elm
+++ src/Main.elm
@@ -6,6 +6,7 @@ import Element exposing (..)
 import Element.Input as Input
 import Element.Font as Font
 import Element.Background as Background
+import Element.Border as Border
 
 
 type alias Model =
@@ -20,7 +21,9 @@ type Input
 
 
 type Msg
-    = ChangeInput Input
+    = SetInput Input
+    | Increase
+    | Decrease
     | BadInput
 
 
@@ -52,7 +55,17 @@ init =
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        ChangeInput input -> { model | input = input }
+        SetInput input -> { model | input = input }
+
+        Increase ->
+            case model.input of
+                NumberInput number -> { model | input = NumberInput (number + 1) }
+                _ -> model
+
+        Decrease ->
+            case model.input of
+                NumberInput number -> { model | input = NumberInput (number - 1) }
+                _ -> model
 
         BadInput -> model
 
@@ -69,7 +82,7 @@ viewPage model =
     column
         [ centerX ]
         [ viewHeading model
-        , viewInput model
+        , row [ width fill ] [ viewInput model, viewButtons model ]
         , viewOutput model
         ]
 
@@ -88,19 +101,19 @@ viewHeading model =
 
 viewInput : Model -> Element Msg
 viewInput model =
-    Input.text []
+    Input.text [ width (fillPortion 2) ]
         { text = case model.input of
             EmptyInput -> ""
             PartialInput str -> str
             NumberInput n -> String.fromInt n
         , onChange = \s ->
                 case s of
-                    "" -> ChangeInput EmptyInput
-                    "-" -> ChangeInput <| PartialInput s
+                    "" -> SetInput EmptyInput
+                    "-" -> SetInput <| PartialInput s
                     _ ->
                         case String.toInt s of
                             Nothing -> BadInput
-                            Just n -> ChangeInput <| NumberInput n
+                            Just n -> SetInput <| NumberInput n
         , label =
             Input.labelAbove
                 [ Font.color colors.light
@@ -109,6 +122,38 @@ viewInput model =
                 (text "enter a base-10 number")
         , placeholder = Nothing
         }
+
+
+viewButtons : Model -> Element Msg
+viewButtons model =
+    let buttonStyle =
+            [ height (px 40)
+            , width (px 40)
+            , spacing 105
+            , Border.width 2
+            , Border.color colors.light
+            , Border.rounded 5
+            ]
+        label str = 
+            el
+                [ centerX, centerY
+                , Font.family [ Font.monospace ]
+                , Font.color colors.light
+                ]
+                (text str)
+    in row
+        [ width (fillPortion 1)
+        , height fill
+        ]
+        [ Input.button buttonStyle
+            { label = label "+"
+            , onPress = Just Increase
+            }
+        , Input.button buttonStyle
+            { label = label "-"
+            , onPress = Just Decrease
+            }
+        ]
 
 
 viewOutput : Model -> Element Msg
